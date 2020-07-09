d1786d45444238bb3ee9af4cbf94552fff21d63e
Antonio Placido <aplacido1992@gmail.com>
2020-07-09 00:06:03 -0400

    2.4

--- workshop/exercises-P2_tests/exercise-2-4.js
+++ workshop/exercises-P2_tests/exercise-2-4.js
@@ -10,12 +10,22 @@
 // getLetterAtIndex("abc", 0);   // a
 
 function getLetterAtIndex(str, index) {
-  // Your code here
+  if (str === "") {
+    return undefined;
+  } else {
+    return str.charAt(index);
+  }
 }
 
 // Add 6 more test cases
-expect(getLetterAtIndex('hello', 4), 'o');
-expect(getLetterAtIndex('goodbye', 0), 'g');
+expect(getLetterAtIndex("hello", 4), "o");
+expect(getLetterAtIndex("nobody", 0), "n");
+expect(getLetterAtIndex("hello", 4), "o");
+expect(getLetterAtIndex("goodbye", 0), "g");
+expect(getLetterAtIndex("hello", 4), "o");
+expect(getLetterAtIndex("goodbye", 2), "l");
+expect(getLetterAtIndex("hello", 6), "o");
+expect(getLetterAtIndex("goodbye", 1), "g");
 
 /**
  * -------------------------------------------------------------------
@@ -24,7 +34,7 @@ expect(getLetterAtIndex('goodbye', 0), 'g');
  */
 function expect(result, value) {
   if (result === value) {
-    console.log('✅ Test succeeded');
+    console.log("✅ Test succeeded");
   } else {
     console.log(`⛔️ Expected “${result}” to equal “${value}”`);
   }
