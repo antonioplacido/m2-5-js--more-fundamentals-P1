4c2c5db473d2299f38ce8f6701668564eee345a3
Antonio Placido <aplacido1992@gmail.com>
2020-07-09 00:39:33 -0400

    Question 2.6

--- workshop/exercises-P2_tests/exercise-2-6.js
+++ workshop/exercises-P2_tests/exercise-2-6.js
@@ -18,13 +18,35 @@
 // f(["spoof", 10, 10]); // undefined
 
 function calculator(arr) {
-  // Your code here
+  switch (arr[0]) {
+    case "add":
+      return arr[1] + arr[2];
+      break;
+    case "sub":
+      return arr[1] - arr[2];
+      break;
+    case "mult":
+      return arr[1] * arr[2];
+      break;
+    default:
+      return undefined;
+      break;
+  }
 }
 
+const testArray = ["test"];
+console.log(testArray.calculator);
 // Step 2
 // We need 8 total test cases. The first two is provided.
-expect(calculator(['mult', 2, 4]), 8);
-expect(calculator(['add', 2, 4]), 6);
+expect(calculator(["mult", 2, 4]), 8);
+expect(calculator(["add", 4, 4]), 8);
+expect(calculator(["sub", 12, 4]), 8);
+expect(calculator(["add", 6, 4]), 10);
+expect(calculator(["mult", 5, 4]), 20);
+expect(calculator(["add", 1, 4]), 5);
+expect(calculator(["sub", 3, 4]), -1);
+expect(calculator(["add", 2, 4]), 6);
+expect(calculator(["kickit", 2, 4]), undefined);
 
 /**
  * -------------------------------------------------------------------
@@ -33,7 +55,7 @@ expect(calculator(['add', 2, 4]), 6);
  */
 function expect(result, value) {
   if (result === value) {
-    console.log('✅ Test succeeded');
+    console.log("✅ Test succeeded");
   } else {
     console.log(`⛔️ Expected “${result}” to equal “${value}”`);
   }
