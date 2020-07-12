a5ab002add5a37d452854187dfa20c6866fbb9f9
Antonio Placido <aplacido1992@gmail.com>
2020-07-11 22:28:55 -0400

    Question 3.2

--- workshop/exercises-P3_problems/exercise-3-2/app.js
+++ workshop/exercises-P3_problems/exercise-3-2/app.js
@@ -9,38 +9,53 @@
 // they need to be set here. So that they are referenceable via the the style attribute.
 // initialize the background color for all of the buttons in here. (for loop)
 
+for (let i = 1; i < 5; i++) {
+  const startButton = document.getElementById(`btn-${i}`);
+  startButton.style.opacity = `100`;
+  startButton.style.backgroundColor = `gold`;
+}
+
 function toggleColor(event) {}
 
 intialStatus = document.getElementById("btn-list");
-document.getElementById("btn-list").addEventListener("click", buttonChange);
-
-intialStatus.style.backgroundColor = "gold";
+intialStatus.addEventListener("click", buttonChange);
 
 function buttonChange(event) {
   let button = event.target;
   let buttonid = button.getAttribute("id");
-  //   let status = false;
-  console.log(button);
 
   switch (buttonid) {
     case "btn-1":
-      let status = false;
-      console.log("btn-1.click");
-      button.style.opacity = "0";
-      status = true;
-      console.log(status);
+      if (button.style.opacity > "0") {
+        button.style.opacity = "0";
+        console.log("dissapear!");
+      } else if (button.style.opacity < 1) {
+        button.style.opacity = "1";
+        console.log("reappear!");
+      }
       return;
     case "btn-2":
-      console.log("btn-2.click");
-      button.style.background = "crimson";
+      if (button.style.background !== "crimson") {
+        console.log("btn-2.click");
+        button.style.background = "crimson";
+      } else {
+        button.style.background = "gold";
+        console.log("turning back gold");
+        break;
+      }
       return;
     case "btn-3":
-      console.log("btn-3.click");
-      button.style.background = "lightblue";
+      if (button.style.background !== "lightblue") {
+        console.log("btn-3.click");
+        button.style.background = "lightblue";
+      } else {
+        button.style.background = "gold";
+        console.log("turning back gold");
+      }
       return;
     case "btn-4":
       console.log("btn-4.click");
-      button.classList.add("jitters");
+      button.classList.toggle("jitters");
       return;
     default:
       return undefined;
